47d0ac23a85775d6cb4368644912c60fd73a451d
Brian Farnsworth <farnswor@adobe.com>
2017-01-08 21:12:35 -0700

    another carrier refactor commit

--- dist/Carrier.js
+++ dist/Carrier.js
@@ -10,15 +10,20 @@ class Carrier extends Worker {
     doWork(){
         var creep = this.creep;
         var creepOpts = this.creepOpts;
-        var energySource = this.getClosestHarvesterWithEnergy();
+        var harvesterWithEnergy = this.getClosestHarvesterWithEnergy();
 
         if(!super.doWork()){
-            util.doWorkOtherwise(creep, {
-                workTarget    : energySource,
-                workFunc      : util.getEnergyFromRoomObject.bind(util, creep, energySource),
-                otherwiseFunc : util.depositEnergyForWork.bind(util, creep),
-                polarity      : 'positive'
-            });
+
+            if(harvesterWithEnergy){
+                util.doWorkOtherwise(creep, {
+                    workTarget    : harvesterWithEnergy,
+                    workFunc      : util.getEnergyFromRoomObject.bind(util, creep, harvesterWithEnergy),
+                    otherwiseFunc : util.depositEnergyForWork.bind(util, creep),
+                    polarity      : 'positive'
+                });
+            }else{
+                //harvest?                
+            }
         }
     }
 
@@ -28,16 +33,17 @@ class Carrier extends Worker {
 
             //get energy from storage if there IS energy in storage.  other wise we just need to get energy from harvesters
             if(storage){
-                util.doWorkOtherwise(creep, {
+                util.doWorkOtherwise(this.creep, {
                     workTarget    : storage,
                     workFunc      : util.getEnergyFromStorage.bind(util, this.creep),
                     otherwiseFunc : util.depositEnergyForSpawning.bind(util, this.creep),
                     polarity      : 'positive'
                 });
             }else{
-                util.doWorkOtherwise(creep, {
-                    workTarget    : this.source,
-                    workFunc      : util.harvest.bind(util, creep, creepOpts),
+                var harvesterWithEnergy = this.getClosestHarvesterWithEnergy();
+                util.doWorkOtherwise(this.creep, {
+                    workTarget    : harvesterWithEnergy,
+                    workFunc      : util.getEnergyFromRoomObject.bind(util, this.creep, harvesterWithEnergy),
                     otherwiseFunc : util.depositEnergyForSpawning.bind(util, this.creep),
                     polarity      : 'positive'
                 });
@@ -47,11 +53,12 @@ class Carrier extends Worker {
 
     getClosestHarvesterWithEnergy(){
         var harvestersWithEnergy = util.getCreepsOfType(['harvester'], this.creep.room).filter(h => {
+
             //I could make this a ratio to avoid having creeps go to a marginally closer one that 
             //has very little energy
             //but I get the reverse problem which is skipping a very close creep that has a little bit of energy
             //I feel like eventually I will want a function that takes into account distance and the amount of energy the creep has or something
-            return h.energy > 0;
+            return h.carry.energy > 0;
         });
 
         return this.creep.pos.findClosestByPathUsingCache(harvestersWithEnergy);
