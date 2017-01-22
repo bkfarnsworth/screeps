0e9fc49322aba42af672e7f43184d99b9b3dacb5
Brian Farnsworth <farnswor@adobe.com>
2017-01-22 09:47:47 -0700

    committing energy util

---
+++ GetEnergyUtil.js
@@ -0,0 +1,35 @@
+class GetEnergyUtil {
+   static getCurrentEnergy(target){
+      let currentEnergy;
+
+      if(target instanceof StructureStorage || target instanceof StructureContainer){
+         currentEnergy = 'store[' + RESOURCE_ENERGY + ']';
+      }else if(target instanceof Creep){
+         currentEnergy = 'carry[' + RESOURCE_ENERGY + ']';
+      }else if(target instanceof Resource){
+         currentEnergy = target.amount;
+      }else{
+         currentEnergy = 'energy';
+      }
+
+      return _.get(target, currentEnergy);
+   }
+
+   static getEnergyCapacity(target){
+      let energyCapacity;
+
+      if(target instanceof StructureStorage || target instanceof StructureContainer){
+         energyCapacity = 'storeCapacity';
+      }else if(target instanceof Creep){
+         energyCapacity = 'carryCapacity';
+      }else if(target instanceof Resource){
+         energyCapacity = target.amount;//for dropped resources, it's always 100% of what it is
+      }else{
+         energyCapacity = 'energyCapacity';
+      }
+
+      return _.get(target, energyCapacity);
+   }
+}
+
+module.exports = GetEnergyUtil;
